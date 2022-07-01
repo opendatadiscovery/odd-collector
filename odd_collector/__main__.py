0861b76ceae5490221d8e38d6653853c0b915a51
Alexey <58273234+LehaStira@users.noreply.github.com>
2022-07-02 00:25:11 +0300

    Update __main__.py

--- odd_collector/__main__.py
+++ odd_collector/__main__.py
@@ -12,30 +12,18 @@ logging.basicConfig(
 
 
 try:
-    from odd_collector.adapters.postgresql.adapter import Adapter
 
-    config = PostgreSQLPlugin(type='postgresql',
-                              name='test_psql',
-                              host='localhost',
-                              port=5432,
-                              database='test',
-                              user='test_user',
-                              password='12345')
-    adapter = Adapter(config=config)
+    loop = asyncio.get_event_loop()
 
-    res = adapter.get_data_entities()
-    print(res)
-    #loop = asyncio.get_event_loop()
+    cur_dirname = path.dirname(path.realpath(__file__))
+    config_path = path.join(cur_dirname, "../collector_config.yaml")
+    adapters_path = path.join(cur_dirname, "adapters")
 
-    #cur_dirname = path.dirname(path.realpath(__file__))
-    #config_path = path.join(cur_dirname, "../collector_config.yaml")
-    #adapters_path = path.join(cur_dirname, "adapters")
-
-    #collector = Collector(
-    #    config_path=config_path,
-    #    root_package="odd_collector.adapters",
-    #    plugins_union_type=AvailablePlugin,
-    #)
+    collector = Collector(
+        config_path=config_path,
+        root_package="odd_collector.adapters",
+        plugins_union_type=AvailablePlugin,
+    )
 
     #loop.run_until_complete(collector.register_data_sources())
 
