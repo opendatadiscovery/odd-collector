cd029c87129c0915a43475d176865ac71bdbc21b
dudyshynyura <dudyshyn.yura@gmail.com>
2022-07-02 20:37:56 +0300

    refactoring vertica db

--- odd_collector/adapters/vertica/vertica_db.py
+++ odd_collector/adapters/vertica/vertica_db.py
@@ -13,17 +13,17 @@ class VerticaDB:
             "database": config.database,
         }
 
-    def __get_connection(self):
+    def _connect(self):
         try:
             self.__connection = vertica_python.connect(**self.__connection_info)
             self.__cursor = self.__connection.cursor()
         except Exception as e:
             logging.error(f"Vertica connection error: {e}")
 
-    def __query(self, columns: str, table: str, order_by: str) -> List[tuple]:
-        return self.__execute(f"select {columns} from {table} order by {order_by}")
+    def _query(self, columns: str, table: str, order_by: str) -> List[tuple]:
+        return self._execute(f"select {columns} from {table} order by {order_by}")
 
-    def __execute(self, query: str) -> List[tuple]:
+    def _execute(self, query: str) -> List[tuple]:
         self.__cursor.execute(query)
         return self.__cursor.fetchall()
 
