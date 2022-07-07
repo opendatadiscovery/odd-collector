606eb9a8b9cdc3ef886d00dacb99c4ce51963d6a
edserbin <edserbin@gmail.com>
2022-07-07 13:57:55 +0200

    move database connection logic from adapter.py to mysql_repository.py

--- odd_collector/adapters/mysql/adapter.py
+++ odd_collector/adapters/mysql/adapter.py
@@ -1,30 +1,21 @@
 import logging
 
-import mysql.connector
-from mysql.connector import errorcode
 from odd_models.models import DataEntity, DataEntityList
 from oddrn_generator import MysqlGenerator
 
 from typing import List
 
 from odd_collector_sdk.domain.adapter import AbstractAdapter
-from .mappers import _column_metadata, _column_table, _column_order_by
 from .mappers.tables import map_tables
+from .mysql_repository import MysqlRepository
 
 
 class Adapter(AbstractAdapter):
-    __connection = None
-    __cursor = None
-
     def __init__(self, config) -> None:
-        self.__host = config.host
-        self.__port = config.port
-        self.__database = config.database
-        self.__user = config.user
-        self.__password = config.password
-        self.__ssl_disabled = config.ssl_disabled
+        self.__config = config
+        self.__mysql_repository = MysqlRepository(config)
         self.__oddrn_generator = MysqlGenerator(
-            host_settings=f"{self.__host}", databases=self.__database
+            host_settings=f"{self.__config.host}", databases=self.__config.database
         )
 
     def get_data_source_oddrn(self) -> str:
@@ -32,17 +23,13 @@ class Adapter(AbstractAdapter):
 
     def get_data_entities(self) -> List[DataEntity]:
         try:
-            self.__connect()
-
-            tables = self.__execute(self.__generate_table_metadata_query())
-            columns = self.__query(_column_metadata, _column_table, _column_order_by)
-            self.__disconnect()
+            tables = self.__mysql_repository.get_tables()
+            columns = self.__mysql_repository.get_columns()
             logging.info(f"Load {len(tables)} Datasets DataEntities from database")
-            return map_tables(self.__oddrn_generator, tables, columns, self.__database)
+            return map_tables(self.__oddrn_generator, tables, columns, self.__config.database)
         except Exception:
             logging.error("Failed to load metadata for tables")
             logging.exception(Exception)
-            self.__disconnect()
         return []
 
     def get_data_entity_list(self) -> DataEntityList:
@@ -50,85 +37,3 @@ class Adapter(AbstractAdapter):
             data_source_oddrn=self.get_data_source_oddrn(),
             items=(self.get_data_entities()),
         )
-
-    def __query(self, columns: str, table: str, order_by: str) -> List[tuple]:
-        return self.__execute(f"select {columns} from {table} order by {order_by}")
-
-    def __execute(self, query: str) -> List[tuple]:
-        self.__cursor.execute(query)
-        records = self.__cursor.fetchall()
-        return records
-
-    # replace
-    def __connect(self):
-        try:
-            self.__connection = mysql.connector.connect(
-                host=self.__host,
-                port=self.__port,
-                database=self.__database,
-                user=self.__user,
-                password=self.__password,
-                ssl_disabled=self.__ssl_disabled,
-            )
-            self.__cursor = self.__connection.cursor()
-
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                logging.error("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                logging.error("Database does not exist")
-            else:
-                logging.error(err)
-            raise DBException("Database error")
-        return
-
-    # replace
-    def __disconnect(self):
-        try:
-            if self.__cursor:
-                self.__cursor.close()
-        except Exception:
-            pass
-        try:
-            if self.__connection:
-                self.__connection.close()
-        except Exception:
-            pass
-        return
-
-    def __generate_table_metadata_query(self):
-        return f"""
-        select t.table_catalog,
-               t.table_schema,
-               t.table_name,
-               t.table_type,
-               t.engine,
-               t.version,
-               t.row_format,
-               t.table_rows,
-               t.avg_row_length,
-               t.data_length,
-               t.max_data_length,
-               t.index_length,
-               t.data_free,
-               t.auto_increment,
-               t.create_time,
-               t.update_time,
-               t.check_time,
-               t.table_collation,
-               t.checksum,
-               t.create_options,
-               t.table_comment,
-               v.view_definition
-        from information_schema.tables t
-                 left join information_schema.views v
-                           on t.TABLE_CATALOG = v.TABLE_CATALOG and
-                              t.TABLE_SCHEMA = v.TABLE_SCHEMA and
-                              t.TABLE_NAME = v.TABLE_NAME
-        where t.table_schema = '{self.__database}'
-        order by t.table_catalog, t.table_schema, t.table_name
-        """
-
-
-class DBException(Exception):
-    pass
---
+++ odd_collector/adapters/mysql/mysql_repository.py
@@ -0,0 +1,93 @@
+import logging
+
+import mysql.connector
+from mysql.connector import errorcode
+
+from .mysql_repository_base import MysqlRepositoryBase
+from .mappers import _column_table, _column_order_by
+from .mappers import ColumnMetadataNamedtuple
+
+from typing import List
+
+
+class MysqlRepository(MysqlRepositoryBase):
+
+    def __init__(self, config):
+        self.__host = config.host
+        self.__port = config.port
+        self.__database = config.database
+        self.__user = config.user
+        self.__password = config.password
+        self.__ssl_disabled = config.ssl_disabled
+
+    def get_tables(self):
+        tables = self.__execute(self.__generate_table_metadata_query())
+        return tables
+
+    def get_columns(self):
+        columns = self.__query(ColumnMetadataNamedtuple.get_str_fields(), _column_table, _column_order_by)
+        return columns
+
+    def __query(self, columns: str, table: str, order_by: str) -> List[tuple]:
+        return self.__execute(f"select {columns} from {table} order by {order_by}")
+
+    def __execute(self, query: str) -> List[tuple]:
+        try:
+            mysql_conn_params = {
+                "host": self.__host,
+                "port": self.__port,
+                "database": self.__database,
+                "user": self.__user,
+                "password": self.__password,
+                "ssl_disabled": self.__ssl_disabled
+            }
+            with mysql.connector.connect(**mysql_conn_params) as mysql_conn:
+                with mysql_conn.cursor() as mysql_cur:
+                    mysql_cur.execute(query)
+                    records = mysql_cur.fetchall()
+                    return records
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                logging.error("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                logging.error("Database does not exist")
+            else:
+                logging.error(err)
+            raise DBException("Database error")
+
+    def __generate_table_metadata_query(self):
+        return f"""
+        select t.table_catalog,
+               t.table_schema,
+               t.table_name,
+               t.table_type,
+               t.engine,
+               t.version,
+               t.row_format,
+               t.table_rows,
+               t.avg_row_length,
+               t.data_length,
+               t.max_data_length,
+               t.index_length,
+               t.data_free,
+               t.auto_increment,
+               t.create_time,
+               t.update_time,
+               t.check_time,
+               t.table_collation,
+               t.checksum,
+               t.create_options,
+               t.table_comment,
+               v.view_definition
+        from information_schema.tables t
+                 left join information_schema.views v
+                           on t.TABLE_CATALOG = v.TABLE_CATALOG and
+                              t.TABLE_SCHEMA = v.TABLE_SCHEMA and
+                              t.TABLE_NAME = v.TABLE_NAME
+        where t.table_schema = '{self.__database}'
+        order by t.table_catalog, t.table_schema, t.table_name
+        """
+
+
+class DBException(Exception):
+    pass
