5fded4fd11d1ee13df0715c0a12c823b9a82dbdb
Pavel Makarichev <vixtir90@gmail.com>
2022-07-29 06:27:52 -0700

    fix(mssql): add sql parsing exception handling (#72)

--- odd_collector/adapters/mssql/adapter.py
+++ odd_collector/adapters/mssql/adapter.py
@@ -1,11 +1,14 @@
-import logging
-import pyodbc
+import contextlib
+from typing import List
 
+import pyodbc
+from odd_collector.domain.plugin import MSSQLPlugin
+from odd_collector_sdk.domain.adapter import AbstractAdapter
 from odd_models.models import DataEntity, DataEntityList
 from oddrn_generator import MssqlGenerator
-from odd_collector_sdk.domain.adapter import AbstractAdapter
 
-from .mappers import table_query, column_query
+from .logger import logger
+from .mappers import column_query, table_query
 from .mappers.tables import map_table
 
 
@@ -13,11 +16,13 @@ class Adapter(AbstractAdapter):
     __connection = None
     __cursor = None
 
-    def __init__(self, config) -> None:
+    def __init__(self, config: MSSQLPlugin) -> None:
         # https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-ver15
         # https://github.com/mkleehammer/pyodbc/wiki/Install
         # https://github.com/mkleehammer/pyodbc/wiki/Connecting-to-SQL-Server-from-Linux
         # cat /etc/odbcinst.ini
+
+        # TODO: Encrypt=YES;TrustServerCertificate=YES for 18 ODBC Driver
         self.__data_source: str = (
             f"DRIVER={config.driver};SERVER={config.host};DATABASE={config.database};"
             f"UID={config.user};PWD={config.password}"
@@ -35,7 +40,7 @@ class Adapter(AbstractAdapter):
     def get_data_source_oddrn(self) -> str:
         return self.__oddrn_generator.get_data_source_oddrn()
 
-    def get_data_entities(self) -> list[DataEntity]:
+    def get_data_entities(self) -> List[DataEntity]:
         try:
             self.__connect()
 
@@ -43,38 +48,31 @@ class Adapter(AbstractAdapter):
             columns = self.__execute(column_query)
 
             return map_table(self.__oddrn_generator, tables, columns)
-        except Exception as e:
-            logging.error("Failed to load metadata for tables")
-            logging.exception(e)
+        except Exception:
+            logger.error("Failed to load metadata for tables", exc_info=True)
         finally:
             self.__disconnect()
         return []
 
-    def __execute(self, query: str) -> list[tuple]:
+    def __execute(self, query: str) -> List[tuple]:
         self.__cursor.execute(query)
-        records = self.__cursor.fetchall()
-        return records
+        return self.__cursor.fetchall()
 
-    def __connect(self):
+    def __connect(self) -> None:
         try:
             self.__connection = pyodbc.connect(self.__data_source)
             self.__cursor = self.__connection.cursor()
         except Exception as e:
-            logging.error(e)
-            raise DBException("Database error")
+            raise DBException("Database error") from e
         return
 
-    def __disconnect(self):
-        try:
+    def __disconnect(self) -> None:
+        with contextlib.suppress(Exception):
             if self.__cursor:
                 self.__cursor.close()
-        except Exception:
-            pass
-        try:
+        with contextlib.suppress(Exception):
             if self.__connection:
                 self.__connection.close()
-        except Exception:
-            pass
         return
 
 
