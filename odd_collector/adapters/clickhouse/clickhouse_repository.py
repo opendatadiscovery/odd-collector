436b519131a79bc7ed073827c307857d8585268e
Madina <madina.gafarova@yandex.ru>
2022-08-08 16:06:48 +0300

    Add custom context manager; fetch data in one connection

--- odd_collector/adapters/clickhouse/clickhouse_repository.py
+++ odd_collector/adapters/clickhouse/clickhouse_repository.py
@@ -1,16 +1,15 @@
 import logging
-from abc import ABC, abstractmethod
-from typing import List
+from abc import ABC
+from typing import Dict
+
+import clickhouse_driver
+
+from exception import ClickHouseException
+from .mappers import _table_select, _column_select, _integration_engines_select
 
 
 class ClickHouseRepositoryBase(ABC):
-    @abstractmethod
-    def get_tables(self):
-        pass
-
-    @abstractmethod
-    def get_columns(self):
-        pass
+    pass
 
 
 class ClickHouseRepository(ClickHouseRepositoryBase):
@@ -21,5 +20,60 @@ class ClickHouseRepository(ClickHouseRepositoryBase):
         self.__user = config.user
         self.__password = config.password
 
-    def __execute(self, query: str) -> List[tuple]:
-        pass
+    def execute(self) -> Dict:
+        clickhouse_conn_params = {
+            "host": self.__host,
+            "port": self.__port,
+            "database": self.__database,
+            "user": self.__user,
+            "password": self.__password,
+        }
+        with ClickHouseManagerConnection(clickhouse_conn_params) as cursor:
+            query_params = {"database": clickhouse_conn_params["database"]}
+
+            logging.info("Get tables")
+
+            cursor.execute(_table_select, query_params)
+            tables = cursor.fetchall()
+
+            logging.info("Get columns")
+            cursor.execute(_column_select, query_params)
+            columns = cursor.fetchall()
+
+            logging.info("Get integration engines")
+            cursor.execute(_integration_engines_select, query_params)
+            integration_engines = cursor.fetchall()
+            return {
+                "tables": tables,
+                "columns": columns,
+                "integration_engines": integration_engines
+            }
+
+
+class ClickHouseManagerConnection:
+    def __init__(self, conn_params: Dict[str, str]):
+        self.__conn_params = conn_params
+
+    def __enter__(self):
+        self.__clickhouse_conn = clickhouse_driver.connect(**self.__conn_params)
+        self.clickhouse_cursor = self.__clickhouse_conn.cursor()
+        return self.clickhouse_cursor
+
+    def __exit__(self, *args):
+        logging.info("Try to close cursor")
+        try:
+            if self.clickhouse_cursor:
+                self.clickhouse_cursor.close()
+        except Exception as exc:
+            logging.error(f"Cannot close cursor: {exc}")
+            raise ClickHouseException("Cannot close clickhouse cursor")
+
+        logging.info("Try to close conneciton")
+        try:
+            if self.__clickhouse_conn:
+                self.__clickhouse_conn.close()
+        except Exception as exc:
+            logging.error(f"Cannot close connection: {exc}")
+            raise ClickHouseException("Cannot close clickhouse conneciton")
+
+        logging.info("ClickHouse resource has been released")
---
+++ odd_collector/adapters/clickhouse/exception.py
@@ -0,0 +1,2 @@
+class ClickHouseException(Exception):
+    pass
