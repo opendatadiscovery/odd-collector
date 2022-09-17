a7e6ee3b4676006189fee85ea92f50dc4cef8515
Uladzislau Lukashou <ulukashou@provectus.com>
2022-09-17 17:32:30 +0200

    transformer field for views added in

--- odd_collector/adapters/superset/mappers/datasets.py
+++ odd_collector/adapters/superset/mappers/datasets.py
@@ -3,6 +3,7 @@ from odd_models.models import DataEntity, DataEntityType, DataSet
 
 from oddrn_generator.generators import SupersetGenerator
 from .columns import map_column
+from odd_collector.domain.utils import extract_transformer_data
 from ..domain.dataset import Dataset
 
 
@@ -17,19 +18,22 @@ def create_dataset(oddrn_generator, dataset: Dataset):
 
 
 def map_table(oddrn_generator: SupersetGenerator, dataset: Dataset) -> DataEntity:
-    if dataset.kind == "virtual":
-        _type = DataEntityType.VIEW
-    else:
-        _type = DataEntityType.TABLE
     try:
-        return DataEntity(
+        data_entity = DataEntity(
             oddrn=dataset.get_oddrn(oddrn_generator),
             owner=dataset.owner,
             description=dataset.description,
             metadata=dataset.metadata,
             name=dataset.name,
-            type=_type,
+            type=DataEntityType.UNKNOWN,
             dataset=create_dataset(oddrn_generator, dataset),
         )
+        if dataset.kind == "virtual":
+            data_entity.type = DataEntityType.VIEW
+            data_entity.data_transformer = extract_transformer_data(dataset.metadata[0].metadata.get('sql'),
+                                                                    oddrn_generator, 'datasets')
+        else:
+            data_entity.type = DataEntityType.TABLE
+        return data_entity
     except Exception as e:
         raise MappingDataError(f"Mapping table {dataset.name} failed") from e
