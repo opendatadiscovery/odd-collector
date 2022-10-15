7d42856c196fc37cb5220ef17717ee68c39e4783
Uladzislau Lukashou <ulukashou@provectus.com>
2022-10-15 15:43:55 +0200

    chore: move mapping under the class

--- odd_collector/adapters/redash/domain/datasource.py
+++ odd_collector/adapters/redash/domain/datasource.py
@@ -9,3 +9,12 @@ class DataSource:
     options: Dict[str, Any]
     type: str
     id: int
+
+    @staticmethod
+    def from_response(node: Dict[str, Any]):
+        return DataSource(name=node['name'],
+                          syntax=node['syntax'],
+                          options=node['options'],
+                          type=node['type'],
+                          id=node['id']
+                          )
--- odd_collector/adapters/redash/domain/query.py
+++ odd_collector/adapters/redash/domain/query.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional, List
+from typing import Optional, List, Dict, Any
 
 
 @dataclass
@@ -9,6 +9,14 @@ class Query:
     data_source_id: str
     name: str
     columns: Optional[List[str]] = None
+
+    @staticmethod
+    def from_response(node: Dict[str, Any]):
+        return Query(id=node['id'],
+                     query=node['query'],
+                     data_source_id=node['data_source_id'],
+                     name=node['name']
+                     )
 
     def get_oddrn(self, oddrn_generator):
         oddrn_generator.set_oddrn_paths(
