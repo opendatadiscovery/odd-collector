c32f901227db8f79ddfaaafc8158538862a4c821
Pavel Mackarichev <vixtir90@gmail.com>
2022-11-10 11:30:37 +0300

    chore(reshift): Able to set schemas to parse

--- odd_collector/adapters/redshift/adapter.py
+++ odd_collector/adapters/redshift/adapter.py
@@ -1,4 +1,5 @@
 import logging
+import traceback
 from typing import List
 
 import psycopg2
@@ -19,20 +20,23 @@ from .mappers import (
 )
 from .mappers.metadata import MetadataColumns, MetadataTables
 from .mappers.tables import map_table
+from ...domain.plugin import RedshiftPlugin
+from .logger import logger
 
 
 class Adapter(AbstractAdapter):
     __connection = None
     __cursor = None
 
-    def __init__(self, config) -> None:
+    def __init__(self, config: RedshiftPlugin) -> None:
         self.__host = config.host
         self.__port = config.port
         self.__database = config.database
         self.__user = config.user
         self.__password = config.password
+        self.__schemas = config.schemas
 
-        self._data_source = f"postgresql://{self.__user}:{self.__password}@{self.__host}:{self.__port}/{self.__database}?connect_timeout=10"
+        self._data_source = f"postgresql://{self.__user}:{self.__password.get_secret_value()}@{self.__host}:{self.__port}/{self.__database}?connect_timeout=10"
         self.__oddrn_generator = RedshiftGenerator(
             host_settings=f"{self.__host}", databases=self.__database
         )
@@ -41,27 +45,29 @@ class Adapter(AbstractAdapter):
         return self.__oddrn_generator.get_data_source_oddrn()
 
     def get_data_entities(self) -> List[DataEntity]:
+        logger.debug("start fetching data")
+        logger.debug(f'Schemas for filter: {self.__schemas or "Were not set"}')
         try:
             self.__connect()
 
             mtables: MetadataTables = MetadataTables(
-                self.__execute(MetadataNamedtuple_QUERY),
-                self.__execute(MetadataNamedtupleAll_QUERY),
-                self.__execute(MetadataNamedtupleRedshift_QUERY),
-                self.__execute(MetadataNamedtupleExternal_QUERY),
-                self.__execute(MetadataNamedtupleInfo_QUERY),
+                self.__execute(MetadataNamedtuple_QUERY(self.__schemas)),
+                self.__execute(MetadataNamedtupleAll_QUERY(self.__schemas)),
+                self.__execute(MetadataNamedtupleRedshift_QUERY(self.__schemas)),
+                self.__execute(MetadataNamedtupleExternal_QUERY(self.__schemas)),
+                self.__execute(MetadataNamedtupleInfo_QUERY(self.__schemas)),
             )
 
             mcolumns: MetadataColumns = MetadataColumns(
-                self.__execute(ColumnMetadataNamedtuple_QUERY),
-                self.__execute(ColumnMetadataNamedtupleRedshift_QUERY),
-                self.__execute(ColumnMetadataNamedtupleExternal_QUERY),
+                self.__execute(ColumnMetadataNamedtuple_QUERY(self.__schemas)),
+                self.__execute(ColumnMetadataNamedtupleRedshift_QUERY(self.__schemas)),
+                self.__execute(ColumnMetadataNamedtupleExternal_QUERY(self.__schemas)),
             )
 
             primary_keys = self.__execute(PrimaryKeys_QUERY)
 
             self.__disconnect()
-            logging.info(
+            logger.debug(
                 f"Load {len(mtables.items)} Datasets DataEntities from database"
             )
 
@@ -69,8 +75,7 @@ class Adapter(AbstractAdapter):
                 self.__oddrn_generator, mtables, mcolumns, primary_keys, self.__database
             )
         except Exception as e:
-            logging.error("Failed to load metadata for tables")
-            logging.exception(e)
+            logger.error("Failed to load metadata for tables", exc_info=True)
             self.__disconnect()
         return []
 
@@ -82,15 +87,15 @@ class Adapter(AbstractAdapter):
 
     def __execute(self, query: str) -> List[tuple]:
         self.__cursor.execute(query)
-        records = self.__cursor.fetchall()
-        return records
+        return self.__cursor.fetchall()
 
     def __connect(self):
         try:
             self.__connection = psycopg2.connect(self._data_source)
             self.__cursor = self.__connection.cursor()
         except psycopg2.Error as err:
-            logging.error(err)
+            logger.error(err)
+            logger.debug(traceback.format_exc())
             raise DBException("Database error")
         return
 
--- odd_collector/adapters/redshift/mappers/__init__.py
+++ odd_collector/adapters/redshift/mappers/__init__.py
@@ -1,4 +1,7 @@
 from collections import namedtuple
+from typing import Optional, List, Dict, Any
+from psycopg2 import sql
+from psycopg2.extensions import AsIs
 
 _METADATA_SCHEMA_URL_PREFIX: str = (
     "https://raw.githubusercontent.com/opendatadiscovery/opendatadiscovery-specification/main/specification/"
@@ -51,23 +54,44 @@ _data_set_field_metadata_excluded_keys_redshift: set = {
     "remarks",
 }
 
+
+def filter_schemas_params(schemas: Optional[List[str]]) -> Dict[str, Any]:
+    predicate = "in" if schemas else "not in"
+    schemas = schemas or [
+        "pg_toast",
+        "pg_internal",
+        "catalog_history",
+        "pg_catalog",
+        "information_schema",
+    ]
+
+    return {
+        "predicate": sql.Literal(AsIs(predicate)),
+        "schemas": sql.Literal(AsIs(", ".join(f"'{s}'" for s in schemas))),
+    }
+
+
 # Metadata Tables
 MetadataNamedtuple = namedtuple(
     typename="MetadataNamedtuple",
     field_names="table_catalog, table_schema, table_name, table_type, remarks",
 )
-MetadataNamedtuple_QUERY = """
-select 
-    table_catalog, table_schema, table_name, table_type, remarks
-from 
-    pg_catalog.svv_tables
-where 
-    table_schema not in ('pg_toast', 'pg_internal', 'catalog_history', 'pg_catalog', 'information_schema')
-    and table_schema not like 'pg_temp_%'
-    and table_type in ('BASE TABLE', 'VIEW')
-order by 
-    table_catalog, table_schema, table_name
-"""
+
+
+def MetadataNamedtuple_QUERY(schemas: Optional[List[str]]):
+    return sql.SQL(
+        """
+        select
+            table_catalog, table_schema, table_name, table_type, remarks
+        from
+            pg_catalog.svv_tables
+        where table_schema {predicate} ( {schemas} )
+            and table_schema not like 'pg_temp_%'
+            and table_type in ('BASE TABLE', 'VIEW')
+        order by
+            table_catalog, table_schema, table_name
+    """
+    ).format(**filter_schemas_params(schemas))
 
 
 # Metadata Tables All
@@ -75,28 +99,33 @@ MetadataNamedtupleAll = namedtuple(
     typename="MetadataNamedtupleAll",
     field_names="database_name, schema_name, table_name, table_type, table_owner, table_creation_time, view_ddl",
 )
-MetadataNamedtupleAll_QUERY = """
-select 
-     (current_database())::character varying(128)             as database_name
-     , n.nspname                                                as schema_name
-     , c.relname                                                as table_name
-     , case when c.relkind = 'v' then 'VIEW' else 'TABLE' end   as table_type
-     , pg_get_userbyid(c.relowner)                              as table_owner
-     , relcreationtime                                          as table_creation_time
-     , case
-           when relkind = 'v' then
-               coalesce(pg_get_viewdef(c.reloid, true), '') end as view_ddl
-from 
-    pg_catalog.pg_class_info c
-left join 
-    pg_catalog.pg_namespace n on n.oid = c.relnamespace
-where 
-    c.relkind in ('r', 'v')
-    and n.nspname not like 'pg_temp_%'
-    and n.nspname not in ('pg_toast', 'pg_internal', 'catalog_history', 'pg_catalog', 'information_schema')
-order by 
-    n.nspname, c.relname
-"""
+
+
+def MetadataNamedtupleAll_QUERY(schemas: Optional[List[str]]):
+    return sql.SQL(
+        """
+        select
+             (current_database())::character varying(128)             as database_name
+             , n.nspname                                                as schema_name
+             , c.relname                                                as table_name
+             , case when c.relkind = 'v' then 'VIEW' else 'TABLE' end   as table_type
+             , pg_get_userbyid(c.relowner)                              as table_owner
+             , relcreationtime                                          as table_creation_time
+             , case
+                   when relkind = 'v' then
+                       coalesce(pg_get_viewdef(c.reloid, true), '') end as view_ddl
+        from
+            pg_catalog.pg_class_info c
+        left join
+            pg_catalog.pg_namespace n on n.oid = c.relnamespace
+        where
+            c.relkind in ('r', 'v')
+            and n.nspname not like 'pg_temp_%'
+            and n.nspname {predicate} ( {schemas} )
+        order by
+            n.nspname, c.relname
+        """
+    ).format(**filter_schemas_params(schemas))
 
 
 # Metadata Tables Redshift
@@ -104,17 +133,22 @@ MetadataNamedtupleRedshift = namedtuple(
     typename="MetadataNamedtupleRedshift",
     field_names="database_name, schema_name, table_name, table_type, table_acl, remarks",
 )
-MetadataNamedtupleRedshift_QUERY = f"""
-select 
-    database_name, schema_name, table_name, table_type, table_acl, remarks
-from 
-    pg_catalog.svv_redshift_tables
-where 
-    schema_name not in ('pg_toast', 'pg_internal', 'catalog_history', 'pg_catalog', 'information_schema')
-    and schema_name not like 'pg_temp_%'
-order by
-    database_name, schema_name, table_name
+
+
+def MetadataNamedtupleRedshift_QUERY(schemas: Optional[List[str]]):
+    return sql.SQL(
+        """
+        select
+            database_name, schema_name, table_name, table_type, table_acl, remarks
+        from
+            pg_catalog.svv_redshift_tables
+        where
+            schema_name {predicate} ( {schemas} )
+            and schema_name not like 'pg_temp_%'
+        order by
+            database_name, schema_name, table_name
 """
+    ).format(**filter_schemas_params(schemas))
 
 
 # Metadata Tables External
@@ -123,20 +157,25 @@ MetadataNamedtupleExternal = namedtuple(
     field_names="databasename, schemaname, tablename, location, input_format, output_format, "
     "serialization_lib, serde_parameters, compressed, parameters, tabletype",
 )
-MetadataNamedtupleExternal_QUERY = """
-select 
-    (current_database())::character varying(128) as databasename, 
-    schemaname, tablename, location, 
-    input_format, output_format, serialization_lib, 
-    serde_parameters, compressed, parameters, tabletype
-from 
-    pg_catalog.svv_external_tables 
-where 
-    schemaname not in ('pg_toast', 'pg_internal', 'catalog_history', 'pg_catalog', 'information_schema') 
-    and schemaname not like 'pg_temp_%'
-order by 
-    schemaname, tablename
-"""
+
+
+def MetadataNamedtupleExternal_QUERY(schemas: Optional[List[str]]):
+    return sql.SQL(
+        """
+        select
+            (current_database())::character varying(128) as databasename,
+            schemaname, tablename, location,
+            input_format, output_format, serialization_lib,
+            serde_parameters, compressed, parameters, tabletype
+        from
+            pg_catalog.svv_external_tables
+        where
+            schemaname {predicate} ({schemas})
+            and schemaname not like 'pg_temp_%'
+        order by
+            schemaname, tablename
+    """
+    ).format(**filter_schemas_params(schemas))
 
 
 # Metadata Tables Info
@@ -146,20 +185,26 @@ MetadataNamedtupleInfo = namedtuple(
     "sortkey_num, size, pct_used, empty, unsorted, stats_off, tbl_rows, skew_sortkey1, skew_rows, "
     "estimated_visible_rows, risk_event, vacuum_sort_benefit",
 )
-MetadataNamedtupleInfo_QUERY = """
-select 
-    database, schema, table_id, "table", encoded, diststyle,
-    sortkey1, max_varchar, trim(sortkey1_enc) sortkey1_enc, sortkey_num, 
-    size, pct_used, empty, unsorted, stats_off, tbl_rows, skew_sortkey1, skew_rows, estimated_visible_rows,
-    risk_event, vacuum_sort_benefit
-from
-    pg_catalog.svv_table_info 
-where
-    schema not in ('pg_toast', 'pg_internal', 'catalog_history', 'pg_catalog', 'information_schema') 
-    and schema not like 'pg_temp_%'
-order by
-    database, schema, "table"
-"""
+
+
+def MetadataNamedtupleInfo_QUERY(schemas: Optional[List[str]]):
+    return sql.SQL(
+        """
+        select
+            database, schema, table_id, "table", encoded, diststyle,
+            sortkey1, max_varchar, trim(sortkey1_enc) sortkey1_enc, sortkey_num,
+            size, pct_used, empty, unsorted, stats_off, tbl_rows, skew_sortkey1, skew_rows, estimated_visible_rows,
+            risk_event, vacuum_sort_benefit
+        from
+            pg_catalog.svv_table_info
+        where
+            schema {predicate} ({schemas})
+            and schema not like 'pg_temp_%'
+        order by
+            database, schema, "table"
+        """
+    ).format(**filter_schemas_params(schemas))
+
 
 # Metadata Columns
 ColumnMetadataNamedtuple = namedtuple(
@@ -167,24 +212,29 @@ ColumnMetadataNamedtuple = namedtuple(
     field_names="database_name, schema_name, table_name, column_name, ordinal_position, column_default, "
     "is_nullable, data_type, character_maximum_length, numeric_precision, numeric_scale, remarks",
 )
-ColumnMetadataNamedtuple_QUERY = """
-select 
-    c.database_name, c.schema_name, c.table_name, c.column_name, 
-    c.ordinal_position, c.column_default, c.is_nullable, c.data_type, 
-    c.character_maximum_length, c.numeric_precision, c.numeric_scale, c.remarks
-from 
-    pg_catalog.svv_all_columns as c
-join 
-    pg_catalog.svv_tables as t on (
-        t.table_catalog = c.database_name and t.table_schema = c.schema_name and t.table_name = c.table_name
-    ) 
-where 
-    c.schema_name not in ('pg_toast', 'pg_internal', 'catalog_history', 'pg_catalog', 'information_schema')
-    and c.schema_name not like 'pg_temp_%'
-    and t.table_type in ('BASE TABLE', 'VIEW') 
-order by 
-    c.database_name, c.schema_name, c.table_name, c.ordinal_position
-"""
+
+
+def ColumnMetadataNamedtuple_QUERY(schemas: Optional[List[str]]):
+    return sql.SQL(
+        """
+        select
+            c.database_name, c.schema_name, c.table_name, c.column_name,
+            c.ordinal_position, c.column_default, c.is_nullable, c.data_type,
+            c.character_maximum_length, c.numeric_precision, c.numeric_scale, c.remarks
+        from
+            pg_catalog.svv_all_columns as c
+        join
+            pg_catalog.svv_tables as t on (
+                t.table_catalog = c.database_name and t.table_schema = c.schema_name and t.table_name = c.table_name
+            )
+        where
+            c.schema_name {predicate} ({schemas})
+            and c.schema_name not like 'pg_temp_%'
+            and t.table_type in ('BASE TABLE', 'VIEW')
+        order by
+            c.database_name, c.schema_name, c.table_name, c.ordinal_position
+        """
+    ).format(**filter_schemas_params(schemas))
 
 
 # Metadata Columns Redshift
@@ -193,19 +243,24 @@ ColumnMetadataNamedtupleRedshift = namedtuple(
     field_names="database_name, schema_name, table_name, column_name, ordinal_position, data_type, column_default, "
     "is_nullable, encoding, distkey, sortkey, column_acl, remarks",
 )
-ColumnMetadataNamedtupleRedshift_QUERY = """
-select 
-    database_name, schema_name, table_name, column_name, 
-    ordinal_position, data_type, column_default, is_nullable, 
-    encoding, distkey, sortkey, column_acl, remarks
-from
-    pg_catalog.svv_redshift_columns 
-where
-    schema_name not in ('pg_toast', 'pg_internal', 'catalog_history', 'pg_catalog', 'information_schema')
-    and schema_name not like 'pg_temp_%'
-order by 
-    database_name, schema_name, table_name, ordinal_position
-"""
+
+
+def ColumnMetadataNamedtupleRedshift_QUERY(schemas: Optional[List[str]]):
+    return sql.SQL(
+        """
+        select
+            database_name, schema_name, table_name, column_name,
+            ordinal_position, data_type, column_default, is_nullable,
+            encoding, distkey, sortkey, column_acl, remarks
+        from
+            pg_catalog.svv_redshift_columns
+        where
+            schema_name {predicate} ({schemas})
+            and schema_name not like 'pg_temp_%'
+        order by
+            database_name, schema_name, table_name, ordinal_position
+        """
+    ).format(**filter_schemas_params(schemas))
 
 
 # Metadata Columns External
@@ -213,18 +268,24 @@ ColumnMetadataNamedtupleExternal = namedtuple(
     typename="ColumnMetadataNamedtupleExternal",
     field_names="databasename, schemaname, tablename, columnname, external_type, columnnum, part_key, is_nullable",
 )
-ColumnMetadataNamedtupleExternal_QUERY = """
-select 
-    (current_database())::character varying(128) as databasename,
-    schemaname, tablename, columnname, external_type, columnnum, part_key, is_nullable
-from 
-    pg_catalog.svv_external_columns
-where 
-    schemaname not in ('pg_toast', 'pg_internal', 'catalog_history', 'pg_catalog', 'information_schema') 
-    and schemaname not like 'pg_temp_%'
-order by 
-    schemaname, tablename, columnnum
-"""
+
+
+def ColumnMetadataNamedtupleExternal_QUERY(schemas: Optional[List[str]]):
+    return sql.SQL(
+        """
+        select
+            (current_database())::character varying(128) as databasename,
+            schemaname, tablename, columnname, external_type, columnnum, part_key, is_nullable
+        from
+            pg_catalog.svv_external_columns
+        where
+            schemaname {predicate} ({schemas})
+            and schemaname not like 'pg_temp_%'
+        order by
+            schemaname, tablename, columnnum
+    """
+    ).format(**filter_schemas_params(schemas))
+
 
 # Primary Keys Query
 PrimaryKeys_QUERY = """
