31bd450e365ddc8ca38cf7dfe7f9ec95725dfff7
Uladzislau Lukashou <ulukashou@provectus.com>
2023-01-24 02:06:01 +0100

    feat: add column types

--- odd_collector/adapters/databricks_lakehouse/mappers/column.py
+++ odd_collector/adapters/databricks_lakehouse/mappers/column.py
@@ -1,6 +1,13 @@
 from oddrn_generator import DatabricksLakehouseGenerator
 from typing import Dict, Any
 from odd_models.models import DataSetField, DataSetFieldType, Type
+from .types import TYPES_SQL_TO_ODD
+
+
+def split_by_braces(value: str) -> str:
+    if isinstance(value, str):
+        return value.split("(")[0].split("<")[0]
+    return value
 
 
 def map_column(
@@ -12,7 +19,7 @@ def map_column(
         oddrn=oddrn_generator.get_oddrn_by_path("columns", name),
         name=name,
         type=DataSetFieldType(
-            type=Type.TYPE_UNKNOWN,
+            type=TYPES_SQL_TO_ODD.get(split_by_braces(_type), Type.TYPE_UNKNOWN),
             logical_type=_type,
             is_nullable=False,
         ),
