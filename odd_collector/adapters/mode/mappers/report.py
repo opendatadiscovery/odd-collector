0e45be83e9a2f3436ab5aeeb4f1f36e7b0977313
edserbin <edserbin@gmail.com>
2023-01-26 16:40:46 +0200

    complete mapper for report

--- odd_collector/adapters/mode/mappers/report.py
+++ odd_collector/adapters/mode/mappers/report.py
@@ -1,11 +1,51 @@
-from odd_models.models import DataEntity
+from odd_models.models import DataEntity, DataEntityType, DataConsumer
 
+from .metadata import map_report_metadata
 from ..domain.report import Report
 from ..generator import ModeGenerator
 
+# list of returned values of adapter:
+# https://mode.com/developer/api-reference/management/data-sources/
+DRIVER_MAPPING = {
+    "jdbc:athena": "athena",
+    "jdbc:bigquery": "bigquery",
+    "jdbc:databricks": "databricks",
+    "jdbc:transform": "transform",
+    "jdbc:druid": "druid",
+    "jdbc:hive": "hive",
+    "jdbc:impala": "impala",
+    "jdbc:mysql": "mysql",
+    "jdbc:oracle": "oracle",
+    "jdbc:oracleadb": "oracle",
+    "jdbc:postgresql": "postgresql",
+    "jdbc:presto": "presto",
+    "jdbc:redshift": "redshift",
+    "jdbc:snowflake": "snowflake",
+    "jdbc:spark": "spark",
+    "jdbc:sqlserver": "mssql",
+    "jdbc:treasuredata": "treasuredata",
+    "jdbc:vertica": "vertica",
+}
 
-# TODO: complete me
+
 def map_report(
         oddrn_generator: ModeGenerator, report: Report
 ) -> DataEntity:
-    return Report
+    inputs = set()
+    for source in report.get_report_db_sources():
+        host = source.server if source.server else report.host
+        database = source.database if source.database else report.database
+        driver = DRIVER_MAPPING[report.adapter]
+        inputs.add(f"//{driver}/host/{host}/databases/{database}/schemas/{source.schema}/tables/{source.table}")
+
+    entity = DataEntity(
+        oddrn=report.get_oddrn(oddrn_generator),
+        name=report.name,
+        metadata=[map_report_metadata(report)],
+        tags=None,
+        type=DataEntityType.DASHBOARD,
+        updated_at=report.updated_at,
+        created_at=report.created_at,
+        data_consumer=DataConsumer(inputs=inputs)
+    )
+    return entity
