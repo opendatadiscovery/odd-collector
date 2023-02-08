92ccd0a5d326c6818c7082493db1c5940d7d6d79
Pavel Mackarichev <vixtir90@gmail.com>
2023-02-08 14:10:43 +0300

    chore: update sdk

--- odd_collector/__main__.py
+++ odd_collector/__main__.py
@@ -1,6 +1,8 @@
+import traceback
 from pathlib import Path
 
 from odd_collector_sdk.collector import Collector
+from odd_collector_sdk.logger import logger
 
 from odd_collector.domain.plugin import PLUGIN_FACTORY
 
@@ -9,9 +11,13 @@ from .version import print_version
 if __name__ == "__main__":
     print_version()
 
-    collector = Collector(
-        config_path=Path().cwd() / "collector_config.yaml",
-        root_package="odd_collector.adapters",
-        plugin_factory=PLUGIN_FACTORY,
-    )
-    collector.run()
+    try:
+        collector = Collector(
+            config_path=Path().cwd() / "collector_config.yaml",
+            root_package="odd_collector.adapters",
+            plugin_factory=PLUGIN_FACTORY,
+        )
+        collector.run()
+    except Exception as e:
+        logger.debug(traceback.format_exc())
+        logger.error(e)
