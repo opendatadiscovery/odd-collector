abcd18b6ee8ae5ab2bde24abd59f3df46393c7b2
Pavel Makarichev <vixtir90@gmail.com>
2023-03-04 18:31:56 +0300

    feat(clickhouse): using clickhouse_connect (#157)
    * build: action for test image

--- odd_collector/adapters/clickhouse/adapter.py
+++ odd_collector/adapters/clickhouse/adapter.py
@@ -1,12 +1,10 @@
 from typing import List
 
 from odd_collector_sdk.domain.adapter import AbstractAdapter
-from odd_collector_sdk.errors import DataSourceError
 from odd_models.models import DataEntity, DataEntityList
 from oddrn_generator import ClickHouseGenerator
 
 from ...domain.plugin import ClickhousePlugin
-from .logger import logger
 from .mappers.tables import map_table
 from .repository import ClickHouseRepository
 
@@ -23,18 +21,14 @@ class Adapter(AbstractAdapter):
         return self.__oddrn_generator.get_data_source_oddrn()
 
     def get_data_entities(self) -> List[DataEntity]:
-        try:
-            records = self.clickhouse_repository.get_records()
-            return map_table(
-                self.__oddrn_generator,
-                records.tables,
-                records.columns,
-                records.integration_engines,
-                self.__db,
-            )
-        except DataSourceError:
-            logger.error(f"Failed to load metadata", exc_info=True)
-            return []
+        records = self.clickhouse_repository.get_records()
+        return map_table(
+            self.__oddrn_generator,
+            records.tables,
+            records.columns,
+            records.integration_engines,
+            self.__db,
+        )
 
     def get_data_entity_list(self) -> DataEntityList:
         return DataEntityList(
