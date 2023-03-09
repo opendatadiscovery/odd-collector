ec43be2433c3326c326a497d462c3fe796045236
Pavel Mackarichev <vixtir90@gmail.com>
2023-03-09 10:48:41 +0300

    chore: Added more verbose errors reason

--- odd_collector/__main__.py
+++ odd_collector/__main__.py
@@ -1,21 +1,16 @@
-import traceback
 from pathlib import Path
 
 from odd_collector_sdk.collector import Collector
-from odd_collector_sdk.logger import logger
 
 from odd_collector.domain.plugin import PLUGIN_FACTORY
 
 COLLECTOR_PACKAGE = __package__
 CONFIG_PATH = Path().cwd() / "collector_config.yaml"
 
-try:
+if __name__ == "__main__":
     collector = Collector(
         config_path=CONFIG_PATH,
         root_package=COLLECTOR_PACKAGE,
         plugin_factory=PLUGIN_FACTORY,
     )
     collector.run()
-except Exception as e:
-    logger.debug(traceback.format_exc())
-    logger.error(e)
