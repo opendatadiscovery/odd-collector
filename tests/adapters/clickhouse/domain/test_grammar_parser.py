29a088169884e67b658736d9dcaa3cdf952c6cfb
Madina <madina.gafarova@yandex.ru>
2023-04-26 16:23:01 +0200

    Test lexer

---
+++ tests/adapters/clickhouse/domain/test_grammar_parser.py
@@ -0,0 +1,61 @@
+import unittest
+
+from odd_collector.adapters.clickhouse.grammar_parser.parser import parser
+from odd_collector.adapters.clickhouse.grammar_parser.column_type import (
+    ParseType,
+    BasicType,
+    Array,
+    Nested,
+    Field
+)
+from odd_collector.adapters.clickhouse.mappers.columns import NestedColumnsTransformer
+from odd_collector.logger import logger
+
+
+class TestTypeParser(unittest.TestCase):
+    def test_array_type(self):
+        test_type = "Array(Nested(g String))"
+        expected = Array(type=Nested(fields={"g": BasicType(type_name="String")}))
+        tree = parser.parse(test_type)
+        result = NestedColumnsTransformer._traverse_tree(tree)
+
+        self.assertIsInstance(result, Array)
+        self.assertIsInstance(result.type, Nested)
+        self.assertEqual(expected.type.fields['g'].type_name, result.type.fields['g'].type_name)
+        result_length = len(result.type.fields)
+        self.assertEqual(result_length, 1)
+
+    def test_complex_structure(self):
+        test_type = "Array(Nested(UserId UInt64, Username Nested(Name String, Surname String)))"
+        expected = Array(
+            type=Nested(
+                fields={
+                    "UserId": BasicType(type_name="UInt64"),
+                    "Username": Nested(
+                        fields={
+                            "Name": BasicType(type_name="String"),
+                            "Surname": BasicType(type_name="String")
+                        }
+                    )
+                }
+            )
+        )
+        tree = parser.parse(test_type)
+        result = NestedColumnsTransformer._traverse_tree(tree)
+        self.assertIsInstance(result, Array)
+        self.assertIsInstance(result.type, Nested)
+        self.assertIsInstance(result.type.fields["Username"], Nested)
+        self.assertEqual(result.type.fields["UserId"].type_name, expected.type.fields['UserId'].type_name)
+        self.assertEqual(
+            result.type.fields["Username"].fields["Name"].type_name,
+            expected.type.fields['Username'].fields["Name"].type_name
+        )
+        self.assertEqual(
+            result.type.fields["Username"].fields["Surname"].type_name,
+            expected.type.fields['Username'].fields["Surname"].type_name
+        )
+        result_length = len(result.type.fields)
+        self.assertEqual(result_length, 2)
+
+        result_nested_length = len(result.type.fields["Username"].fields)
+        self.assertEqual(result_nested_length, 2)
