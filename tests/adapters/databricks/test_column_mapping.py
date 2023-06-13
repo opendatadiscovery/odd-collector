508d0ba9f8c5c7e94c8d42e945de508a3972ac4e
Madina <madina.gafarova@yandex.ru>
2023-06-13 16:55:52 +0200

    Fix tests: process complex maps

--- tests/adapters/databricks/test_column_mapping.py
+++ tests/adapters/databricks/test_column_mapping.py
@@ -28,12 +28,58 @@ class TestColumnMapping(TestCase):
         )
 
         dataset_fields = build_dataset_field(column, self.oddrn_generator)
-        self.assertEqual(len(dataset_fields), 1)
+        self.assertEqual(len(dataset_fields), 3)
 
         self.assertEqual(dataset_fields[0].parent_field_oddrn, None)
         self.assertEqual(dataset_fields[0].type.type, Type.TYPE_MAP)
         self.assertEqual(dataset_fields[0].type.logical_type, "Map(string, boolean)")
 
+        self.assertEqual(dataset_fields[1].parent_field_oddrn, dataset_fields[0].oddrn)
+        self.assertEqual(dataset_fields[1].oddrn, dataset_fields[0].oddrn + "/keys/Key")
+        self.assertEqual(dataset_fields[1].type.type, Type.TYPE_STRING)
+        self.assertEqual(dataset_fields[1].type.logical_type, "string")
+
+        self.assertEqual(dataset_fields[2].parent_field_oddrn, dataset_fields[0].oddrn)
+        self.assertEqual(dataset_fields[2].oddrn, dataset_fields[0].oddrn + "/keys/Value")
+        self.assertEqual(dataset_fields[2].type.type, Type.TYPE_BOOLEAN)
+        self.assertEqual(dataset_fields[2].type.logical_type, "boolean")
+        
+    def test_build_complex_map_dataset(self):
+        column = DatabricksColumn(
+            name="test",
+            type="map<string,struct<a:int,b:string>>",
+            table_catalog="test",
+            table_schema="test",
+            table_name="test",
+            is_nullable=False
+        )
+
+        dataset_fields = build_dataset_field(column, self.oddrn_generator)
+        self.assertEqual(len(dataset_fields), 5)
+
+        self.assertEqual(dataset_fields[0].parent_field_oddrn, None)
+        self.assertEqual(dataset_fields[0].type.type, Type.TYPE_MAP)
+        self.assertEqual(dataset_fields[0].type.logical_type, "Map(string, Struct(a: int, b: string))")
+
+        self.assertEqual(dataset_fields[1].parent_field_oddrn, dataset_fields[0].oddrn)
+        self.assertEqual(dataset_fields[1].oddrn, dataset_fields[0].oddrn + "/keys/Key")
+        self.assertEqual(dataset_fields[1].type.type, Type.TYPE_STRING)
+        self.assertEqual(dataset_fields[1].type.logical_type, "string")
+
+        self.assertEqual(dataset_fields[2].parent_field_oddrn, dataset_fields[0].oddrn)
+        self.assertEqual(dataset_fields[2].oddrn, dataset_fields[0].oddrn + "/keys/Value")
+        self.assertEqual(dataset_fields[2].type.type, Type.TYPE_STRUCT)
+        self.assertEqual(dataset_fields[2].type.logical_type, "Struct(a: int, b: string)")
+
+        self.assertEqual(dataset_fields[3].parent_field_oddrn, dataset_fields[2].oddrn)
+        self.assertEqual(dataset_fields[3].oddrn, dataset_fields[2].oddrn + "/keys/a")
+        self.assertEqual(dataset_fields[3].type.type, Type.TYPE_INTEGER)
+        self.assertEqual(dataset_fields[3].type.logical_type, "int")
+
+        self.assertEqual(dataset_fields[4].parent_field_oddrn, dataset_fields[2].oddrn)
+        self.assertEqual(dataset_fields[4].oddrn, dataset_fields[2].oddrn + "/keys/b")
+        self.assertEqual(dataset_fields[4].type.type, Type.TYPE_STRING)
+        self.assertEqual(dataset_fields[4].type.logical_type, "string")
 
     def test_build_nested_dataset(self):
         column = DatabricksColumn(
@@ -49,12 +95,12 @@ class TestColumnMapping(TestCase):
         self.assertEqual(len(dataset_fields), 4)
 
         self.assertEqual(dataset_fields[0].parent_field_oddrn, None)
-        self.assertEqual(dataset_fields[0].type.type, Type.TYPE_LIST)
+        self.assertEqual(dataset_fields[0].type.type, Type.TYPE_STRUCT)
         self.assertEqual(dataset_fields[0].type.logical_type, "Struct(a: Struct(b: int, c: Array(int)))")
 
         self.assertEqual(dataset_fields[1].parent_field_oddrn, dataset_fields[0].oddrn)
         self.assertEqual(dataset_fields[1].oddrn, dataset_fields[0].oddrn + "/keys/a")
-        self.assertEqual(dataset_fields[1].type.type, Type.TYPE_LIST)
+        self.assertEqual(dataset_fields[1].type.type, Type.TYPE_STRUCT)
         self.assertEqual(dataset_fields[1].type.logical_type, "Struct(b: int, c: Array(int))")
 
         self.assertEqual(dataset_fields[2].parent_field_oddrn, dataset_fields[1].oddrn)
