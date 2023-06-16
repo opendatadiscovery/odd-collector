7148ffabb8d25e09f7ac4b81bee8c97139295d27
Madina <madina.gafarova@yandex.ru>
2023-06-16 16:30:45 +0200

    Simplify process of dataset field creation

--- odd_collector/adapters/clickhouse/mappers/columns.py
+++ odd_collector/adapters/clickhouse/mappers/columns.py
@@ -9,8 +9,122 @@ from ..domain import Column, NestedColumn
 from ..logger import logger
 from .types import TYPES_SQL_TO_ODD
 from ..grammar_parser.parser import parser, traverse_tree
-from ..grammar_parser.column_type import ParseType, Array, Nested
+from ..grammar_parser.column_type import ParseType, Array, Nested, Map, BasicType
 from ..grammar_parser.exceptions import UnexpectedTypeError
+
+
+def build_dataset_fields(columns: List[Column], oddrn_generator, table_oddrn_path):
+    generated_dataset_fields = []
+    ds_fields_oddrn = {}
+
+    def _build_dataset_fields(column_name: str, column_type: ParseType, parent_oddrn=None):
+        logger.debug(f"Process {column_name} with type {column_type} and parent {parent_oddrn}")
+        column_names = column_name.split('.')
+        if parent_oddrn is None:
+            oddrn = oddrn_generator.get_oddrn_by_path(
+                f"{table_oddrn_path}_columns", column_names[0]
+            )
+        else:
+            oddrn = f"{parent_oddrn}/keys/{column_names[0]}"
+        logger.debug(f"Column {column_names[0]} has oddrn {oddrn}")
+
+        if len(column_names) != 1:
+            logger.debug(f"Process complex column {column_name}")
+
+            if not column_names[0] in ds_fields_oddrn:
+                ds_fields_oddrn[column_names[0]] = oddrn
+                generated_dataset_fields.append(
+                    DataSetField(
+                        oddrn=oddrn,
+                        name=column_names[0],
+                        type=DataSetFieldType(
+                            type=type_to_oddrn_type(column_type),
+                            logical_type=column_type.to_clickhouse_type(),
+                            is_nullable=False,
+                        ),
+                        owner=None
+                    )
+                )
+            else:
+                logger.debug(f"Alrady processed {column_names[0]}")
+            if isinstance(column_type, Array):
+                _build_dataset_fields(column_names[1], column_type.type, ds_fields_oddrn[column_names[0]])
+            else:
+                logger.debug(f"Expect that in complex structure type starts with 'Array'. Started from {column_type}")
+        else:
+            if isinstance(column_type, Map):
+                generated_dataset_fields.append(
+                    DataSetField(
+                        oddrn=oddrn,
+                        name=column_name,
+                        type=DataSetFieldType(
+                            type=Type.TYPE_MAP,
+                            logical_type=column_type.to_clickhouse_type(),
+                            is_nullable=False
+                        ),
+                        parent_field_oddrn=parent_oddrn,
+                        owner=None,
+                    )
+                )
+                _build_dataset_fields("Key", column_type.key_type, oddrn)
+                _build_dataset_fields("Value", column_type.value_type, oddrn)
+            elif isinstance(column_type, Nested):
+                generated_dataset_fields.append(
+                    DataSetField(
+                        oddrn=oddrn,
+                        name=column_name,
+                        type=DataSetFieldType(
+                            type=Type.TYPE_STRUCT,
+                            logical_type=column_type.to_clickhouse_type(),
+                            is_nullable=False
+                        ),
+                        owner=None,
+                        parent_field_oddrn=parent_oddrn
+                    ),
+                )
+                for field_name, field_type in column_type.fields.items():
+                    _build_dataset_fields(field_name, field_type, oddrn)
+            else:
+                odd_type = type_to_oddrn_type(column_type)
+                logger.debug(
+                    f"Column {column_name} has ODD type {odd_type} and logical type {column_type.to_clickhouse_type()}"
+                )
+                generated_dataset_fields.append(
+                    DataSetField(
+                        oddrn=oddrn,
+                        name=column_name,
+                        type=DataSetFieldType(
+                            type=odd_type,
+                            logical_type=column_type.to_clickhouse_type(),
+                            is_nullable=False,
+                        ),
+                        owner=None,
+                        parent_field_oddrn=parent_oddrn,
+                    )
+                )
+    
+    for column in columns:
+        type_tree = parser.parse(column.type)
+        column_type = traverse_tree(type_tree)
+
+        _build_dataset_fields(column.name, column_type)
+
+    return generated_dataset_fields
+
+
+def type_to_oddrn_type(column_type):
+    if isinstance(column_type, Array):
+        return Type.TYPE_LIST
+    elif isinstance(column_type, Nested):
+        return Type.TYPE_STRUCT
+    elif isinstance(column_type, Map):
+        return Type.TYPE_MAP
+    elif isinstance(column_type, BasicType):
+        return TYPES_SQL_TO_ODD.get(column_type.type_name, Type.TYPE_UNKNOWN)
+    elif isinstance(column_type, str):
+        return TYPES_SQL_TO_ODD.get(column_type, Type.TYPE_UNKNOWN)
+    else:
+        return Type.TYPE_UNKNOWN
 
 
 def build_nested_columns(columns: List[Column]) -> List[NestedColumn]:
