31b8253a5b885fef21adf69ea216816b17bd3bb9
Pavel Mackarichev <vixtir90@gmail.com>
2023-07-25 11:54:48 +0300

    type: example

--- odd_collector/__main__.py
+++ odd_collector/__main__.py
@@ -1,39 +1,16 @@
-# from pathlib import Path
+from pathlib import Path
 
-# from odd_collector_sdk.collector import Collector
+from odd_collector_sdk.collector import Collector
 
-# from odd_collector.domain.plugin import PLUGIN_FACTORY
+from odd_collector.domain.plugin import PLUGIN_FACTORY
 
-# COLLECTOR_PACKAGE = __package__
-# CONFIG_PATH = Path().cwd() / "collector_config.yaml"
+COLLECTOR_PACKAGE = __package__
+CONFIG_PATH = Path().cwd() / "collector_config.yaml"
 
-# if __name__ == "__main__":
-#     collector = Collector(
-#         config_path=CONFIG_PATH,
-#         root_package=COLLECTOR_PACKAGE,
-#         plugin_factory=PLUGIN_FACTORY,
-#     )
-#     collector.run()
-
-
-from odd_collector.adapters.superset.adapter import Adapter
-from odd_collector.domain.plugin import SupersetPlugin
-
-config = SupersetPlugin(
-    name="superset_adapter",
-    type="superset",
-    server="http://localhost:8088",
-    username="admin",
-    password="admin",
-)
-
-adapter = Adapter(config)
-
-
-async def main():
-    await adapter.get_data_entity_list()
-
-
-import asyncio
-
-asyncio.run(main())
+if __name__ == "__main__":
+    collector = Collector(
+        config_path=CONFIG_PATH,
+        root_package=COLLECTOR_PACKAGE,
+        plugin_factory=PLUGIN_FACTORY,
+    )
+    collector.run()
