373ed2981c6087127c5021443c47fdaed8646f1a
Mateusz Kulas <mkulas@provectus.com>
2023-09-11 11:05:07 +0200

    Derive from BaseAdapter

--- odd_collector/adapters/redshift/adapter.py
+++ odd_collector/adapters/redshift/adapter.py
@@ -1,6 +1,6 @@
-from odd_collector_sdk.domain.adapter import AbstractAdapter
+from odd_collector_sdk.domain.adapter import BaseAdapter
 from odd_models.models import DataEntity, DataEntityList
-from oddrn_generator import RedshiftGenerator
+from oddrn_generator import RedshiftGenerator, Generator
 
 from ...domain.plugin import RedshiftPlugin
 from .logger import logger
@@ -9,32 +9,36 @@ from .mappers.tables import map_table
 from .repository import RedshiftRepository
 
 
-class Adapter(AbstractAdapter):
+class Adapter(BaseAdapter):
     def __init__(self, config: RedshiftPlugin) -> None:
-        self.__database = config.database
-        self.__repository = RedshiftRepository(config)
-        self.__oddrn_generator = RedshiftGenerator(
-            host_settings=config.host, databases=self.__database
+        super().__init__(config)
+        self.database = config.database
+        self.repository = RedshiftRepository(config)
+
+    def create_generator(self) -> Generator:
+        return RedshiftGenerator(
+            host_settings=self.config.host,
+            databases=self.config.database,
         )
 
     def get_data_source_oddrn(self) -> str:
-        return self.__oddrn_generator.get_data_source_oddrn()
+        return self.generator.get_data_source_oddrn()
 
     def get_data_entities(self) -> list[DataEntity]:
         try:
-            mtables: MetadataTables = self.__repository.get_tables()
-            mcolumns: MetadataColumns = self.__repository.get_columns()
-            primary_keys = self.__repository.get_primary_keys()
+            mtables: MetadataTables = self.repository.get_tables()
+            mcolumns: MetadataColumns = self.repository.get_columns()
+            primary_keys = self.repository.get_primary_keys()
 
             logger.debug(
                 f"Load {len(mtables.items)} Datasets DataEntities from database"
             )
 
             return map_table(
-                self.__oddrn_generator, mtables, mcolumns, primary_keys, self.__database
+                self.generator, mtables, mcolumns, primary_keys, self.database
             )
         except Exception as e:
-            logger.error("Failed to load metadata for tables", exc_info=True)
+            logger.error(f"Failed to load metadata for tables: {e}")
         return []
 
     def get_data_entity_list(self) -> DataEntityList:
