fc3d481da4ae00bf0a2a65c4cf35866f7912966e
Pavel Mackarichev <vixtir90@gmail.com>
2023-10-05 13:18:34 +0300

    chore: print versions

--- odd_collector/__main__.py
+++ odd_collector/__main__.py
@@ -2,12 +2,16 @@ from pathlib import Path
 
 from odd_collector_sdk.collector import Collector
 
+from odd_collector import get_version
 from odd_collector.domain.plugin import PLUGIN_FACTORY
+
+from .logger import logger
 
 COLLECTOR_PACKAGE = __package__
 CONFIG_PATH = Path().cwd() / "collector_config.yaml"
 
 if __name__ == "__main__":
+    logger.info(f"Starting collector. Version: {get_version()}")
     collector = Collector(
         config_path=CONFIG_PATH,
         root_package=COLLECTOR_PACKAGE,
